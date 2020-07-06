c7c5284bfe8a9e5fa47f219d71d3d9dbc476d650
SharvariDeshmukh <sharvarideshmukh475@gmail.com>
2020-07-06 23:19:03 +0530

    API user cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//http
+import {HttpClientModule} from '@angular/common/http';
+
+//toastr
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import { ToastrModule } from 'ngx-toastr';
+
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import{
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhone,
+  faDatabase,
+} from "@fortawesome/free-solid-svg-icons";
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() user;
+  faEnvelope=faEnvelope;
+  faPhone = faPhone;
+  faDatabase=faDatabase;
+  faMapMarkedAlt=faMapMarkedAlt;
+  constructor() { }
+ 
+  ngOnInit(): void {
+  }
+
+}
